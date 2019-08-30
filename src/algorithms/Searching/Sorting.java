bb9b5e8b66f2ef33ad569a7d621f29c0a072b836
Adewale Osobu <aosobu@teamapt.com>
2019-08-30 15:29:35 +0100

    added jump search algorithm

---
+++ src/algorithms/Searching/Sorting.java
@@ -0,0 +1,54 @@
+package algorithms.Searching;
+
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Sorting {
+
+    /**
+     * time complexity O(squareroot(n))
+     * space complexity O(1)
+     */
+    public static <T extends Number & Comparable<T>> int jumpSearch(T[] array, T searchKey){
+        //declare local variables
+        int arraySize = array.length;
+        int step = (int) Math.sqrt(Double.parseDouble(String.valueOf(arraySize)));
+        int low = 0;
+        int high = low + step;
+        int index = -1;
+
+        //determining bounds
+        while (high <= arraySize){
+            if(array[low].compareTo(searchKey) == 0) {
+                index = low;
+                break;
+            }
+            if(array[high - 1].compareTo(searchKey) == 0) {
+                index = high - 1;
+                break;
+            }
+
+            if (array[low].compareTo(searchKey) < 0 && array[high - 1].compareTo(searchKey) > 0){
+                for(int i = low + 1; i < high; i++) {
+                    if (array[i].compareTo(searchKey) == 0) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            if(index != -1)
+                 break;
+
+                low = high;
+                high += step;
+        }
+
+        return index;
+    }
+
+
+}
